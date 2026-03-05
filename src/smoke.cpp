a79186e08933072e7af55365c9f02c353f9e3add
Laurence Wilkes <lauriewilkes@hotmail.co.uk>
2026-03-05 10:51:06 +0100

    Dye moved to a different struct to facilitate alternative display of fluid

--- src/smoke.cpp
+++ src/smoke.cpp
@@ -7,149 +7,174 @@ using namespace std;
 
 const int N = 164; // simulation size is N x N voxels
 const int iterGS = 10; // number of iterations of the Gauss--Seidel linear solvers
+const float visc = 0.00005f;        // diffusion for velocities of fluid
+const float diff = 0.000001f;        // diffusion for dye
 
-inline int IX(int i, int j) { return i + (N + 2) * j; }
+struct ScalarField {
+    int side; // in our case side = N + 2
+    vector<float> data;
+    vector<float> tmp;
 
-struct FluidSim {
-    const int size = (N + 2) * (N + 2); // Size of vectors containing velocities/densities. (includes a buffer of one pixel around whole grid)
-    const float diff = 0.000001f;       // diffusion for dyes
-    const float visc = 0.00005f;        // diffusion for velocities of fluid
+    ScalarField(int side) : side(side), data(side * side, 0), tmp(side * side, 0) {}
 
-    vector<float> u, v, u0, v0;
-    vector<float> dr, dg, db, dr0, dg0, db0;
+    void swapTemp() {data.swap(tmp);}
 
-    FluidSim() :
-        u(size, 0), v(size, 0), u0(size, 0), v0(size, 0),
-        dr(size, 0), dr0(size, 0), dg(size, 0), dg0(size, 0), db(size, 0), db0(size, 0) {}
+    inline int IX(int i, int j) const {return i + side * j;}
+    float& operator()(int i, int j) {return data[IX(i, j)];}
+    const float& operator()(int i, int j) const {return data[IX(i, j)];}
+    float& tmpAt(int i, int j) {return tmp[IX(i, j)];}
+    const float& tmpAt(int i, int j) const {return tmp[IX(i, j)];}
 
-    void setBound(int b, vector<float>& x) {
-        for (int i = 1; i <= N; i++) {
-            x[IX(0, i)]     = b == 1 ? -x[IX(1, i)] : x[IX(1, i)];
-            x[IX(N + 1, i)] = b == 1 ? -x[IX(N, i)] : x[IX(N, i)];
-            x[IX(i, 0)]     = b == 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
-            x[IX(i, N + 1)] = b == 2 ? -x[IX(i, N)] : x[IX(i, N)];
+    void setBound(int b) {
+        for (int i = 1; i <= side-2; i++) {
+            data[IX(0, i)]      = b == 1 ? -data[IX(1, i)] : data[IX(1, i)];
+            data[IX(side-1, i)] = b == 1 ? -data[IX(side-2, i)] : data[IX(side-2, i)];
+            data[IX(i, 0)]      = b == 2 ? -data[IX(i, 1)] : data[IX(i, 1)];
+            data[IX(i, side-1)] = b == 2 ? -data[IX(i, side-2)] : data[IX(i, side-2)];
         }
-        x[IX(0, 0)]         = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
-        x[IX(0, N + 1)]     = 0.5 * (x[IX(1, N + 1)] + x[IX(0, N)]);
-        x[IX(N + 1, 0)]     = 0.5 * (x[IX(N, 0)] + x[IX(N + 1, 1)]);
-        x[IX(N + 1, N + 1)] = 0.5 * (x[IX(N, N + 1)] + x[IX(N + 1, N)]);
+        data[IX(0, 0)]           = 0.5 * (data[IX(1, 0)] + data[IX(0, 1)]);
+        data[IX(0, side-1)]      = 0.5 * (data[IX(1, side-1)] + data[IX(0, side-2)]);
+        data[IX(side-1, 0)]      = 0.5 * (data[IX(side-2, 0)] + data[IX(side-1, 1)]);
+        data[IX(side-1, side-1)] = 0.5 * (data[IX(side-2, side-1)] + data[IX(side-1, side-2)]);
     }
 
-    void diffuse(int b, vector<float>& x, vector<float>& x0, float diffCoeff, float dt) {
-        float a = dt * diffCoeff * N * N;
+    void diffuse(int b, float diffCoeff, float dt) {
+        float a = dt * diffCoeff * (side - 2) * (side - 2);
         for (int k = 0; k < iterGS; k++) {
-            for (int j = 1; j <= N; j++) {
-                for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= side - 2; j++) {
+                for (int i = 1; i <= side - 2; i++) {
                     int id = IX(i, j);
-                    x[id] = (x0[id] + a * (
-                        x[IX(i - 1, j)] + x[IX(i + 1, j)] +
-                        x[IX(i, j - 1)] + x[IX(i, j + 1)]
+                    data[id] = (tmp[id] + a * (
+                        data[IX(i - 1, j)] + data[IX(i + 1, j)] +
+                        data[IX(i, j - 1)] + data[IX(i, j + 1)]
                     )) / (1 + 4 * a);
                 }
             }
-            setBound(b, x);
+            setBound(b);
         }
     }
 
-    void advect(int b, vector<float>& d, vector<float>& d0, vector<float>& u, vector<float>& v, float dt) {
-        float dt0 = dt * N;
-        for (int j = 1; j <= N; j++) {
-            for (int i = 1; i <= N; i++) {
+    void advect(int b, const ScalarField& u, const ScalarField& v, float dt) {
+        float dt0 = dt * (side - 2);
+	const float* udat;
+	const float* vdat;
+	if (b == 0) {
+	    udat = u.data.data();
+	    vdat = v.data.data();
+	} else {
+	    udat = u.tmp.data();  // we are required to read tmp when
+	    vdat = v.tmp.data();  // updating u or v themselves
+	}
+        for (int j = 1; j <= side - 2; j++) {
+            for (int i = 1; i <= side - 2; i++) {
                 int id = IX(i, j);
-                float f = i - dt0 * u[id];
-                float g = j - dt0 * v[id];
-                f = clamp(f, 0.5f, N + 0.5f);
-                g = clamp(g, 0.5f, N + 0.5f);
-                int i0 = floor(f), j0 = floor(g);
+                float f = i - dt0 * udat[id];
+                float g = j - dt0 * vdat[id];
+                f = clamp(f, 0.5f, side - 1.5f);
+                g = clamp(g, 0.5f, side - 1.5f);
+                int i0 = (int) f, j0 = (int) g;
                 int i1 = i0 + 1, j1 = j0 + 1;
                 float s1 = f - i0, s0 = 1 - s1;
                 float t1 = g - j0, t0 = 1 - t1;
-                d[id] = s0 * (t0 * d0[IX(i0, j0)] + t1 * d0[IX(i0, j1)])
-                      + s1 * (t0 * d0[IX(i1, j0)] + t1 * d0[IX(i1, j1)]);
+                data[id] = s0 * (t0 * tmp[IX(i0, j0)] + t1 * tmp[IX(i0, j1)])
+			 + s1 * (t0 * tmp[IX(i1, j0)] + t1 * tmp[IX(i1, j1)]);
             }
         }
-        setBound(b, d);
-    } 
+        setBound(b);
+    } // advect
+}; // ScalarField
 
-    void project(vector<float>& u, vector<float>& v, vector<float>& p, vector<float>& div) {
+
+struct FluidSim {
+    ScalarField u, v;
+
+    FluidSim() : u(N + 2), v(N + 2) {}
+
+    void project() {
         float h = 1.0f / N;
         for (int j = 1; j <= N; j++) {
             for (int i = 1; i <= N; i++) {
-                div[IX(i, j)] = -0.5 * h * (
-                    (u[IX(i + 1, j)] - u[IX(i - 1, j)]) +
-                    (v[IX(i, j + 1)] - v[IX(i, j - 1)])
+                v.tmpAt(i, j) = -0.5 * h * (
+                    (u(i + 1, j) - u(i - 1, j)) +
+                    (v(i, j + 1) - v(i, j - 1))
                 );
-                p[IX(i, j)] = 0;
+                u.tmpAt(i, j) = 0;
             }
         }
-        setBound(0, div);
-        setBound(0, p);
+        v.swapTemp(); v.setBound(0); v.swapTemp(); // v.tmp set bound
+        u.swapTemp(); u.setBound(0); u.swapTemp(); // u.tmp set bound
 
         for (int k = 0; k < iterGS; k++) {
             for (int j = 1; j <= N; j++) {
                 for (int i = 1; i <= N; i++) {
-                    p[IX(i, j)] = (
-                        div[IX(i, j)] +
-                        p[IX(i - 1, j)] + p[IX(i + 1, j)] +
-                        p[IX(i, j - 1)] + p[IX(i, j + 1)]
+                    u.tmpAt(i, j) = (
+                        v.tmpAt(i, j) +
+                        u.tmpAt(i - 1, j) + u.tmpAt(i + 1, j) +
+                        u.tmpAt(i, j - 1) + u.tmpAt(i, j + 1)
                     ) / 4;
                 }
             }
-            setBound(0, p);
+	    u.swapTemp(); u.setBound(0); u.swapTemp();
         }
 
         for (int j = 1; j <= N; j++) {
             for (int i = 1; i <= N; i++) {
-                u[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
-                v[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
+                u(i, j) -= 0.5 * (u.tmpAt(i + 1, j) - u.tmpAt(i - 1, j)) / h;
+                v(i, j) -= 0.5 * (u.tmpAt(i, j + 1) - u.tmpAt(i, j - 1)) / h;
             }
         }
-        setBound(1, u);
-        setBound(2, v);
+	u.setBound(1);
+	v.setBound(2);
     }
 
     void velocityStep(float dt) {
-        u.swap(u0); diffuse(1, u, u0, visc, dt);
-        v.swap(v0); diffuse(2, v, v0, visc, dt);
-        project(u, v, u0, v0);
-        u.swap(u0); v.swap(v0);
-        advect(1, u, u0, u0, v0, dt);
-        advect(2, v, v0, u0, v0, dt);
-        project(u, v, u0, v0);
+	u.swapTemp(); u.diffuse(1, visc, dt);
+	v.swapTemp(); v.diffuse(2, visc, dt);
+        project();
+        u.swapTemp(); v.swapTemp();
+	u.advect(1, u, v, dt); // interpolates from
+	v.advect(2, u, v, dt); // tmp buffer as b != 0.
+        project();
     }
+}; // FluidSim
 
-    void densityStep(float dt) {
-        dr.swap(dr0); diffuse(0, dr, dr0, diff, dt); dr.swap(dr0); advect(0, dr, dr0, u, v, dt);
-        dg.swap(dg0); diffuse(0, dg, dg0, diff, dt); dg.swap(dg0); advect(0, dg, dg0, u, v, dt);
-        db.swap(db0); diffuse(0, db, db0, diff, dt); db.swap(db0); advect(0, db, db0, u, v, dt);
-    }
-};
+struct DyeSim {
+    ScalarField dr, dg, db;
+
+    DyeSim() : dr(N + 2), dg(N + 2), db(N + 2) {}
+
+    void densityStep(const ScalarField& u, const ScalarField& v, float dt) {
+        dr.swapTemp(); dr.diffuse(0, diff, dt); dr.swapTemp(); dr.advect(0, u, v, dt);
+        dg.swapTemp(); dg.diffuse(0, diff, dt); dg.swapTemp(); dg.advect(0, u, v, dt);
+        db.swapTemp(); db.diffuse(0, diff, dt); db.swapTemp(); db.advect(0, u, v, dt);
+    } // densityStep
+}; // DyeSim
 
 struct GeyserState {
     float next = 0.0f;
     bool par1 = true, par2 = true;
     int dist = 1;
     float col[3] = {1.0f, 1.0f, 1.0f};
+
+    void resetGeyser(float now) {
+	next = now + 500 + (rand() / (float) RAND_MAX) * 1500;
+	par1 = rand() % 2; // random numbers not guaranteed to be uniform here
+	par2 = rand() % 2;
+	dist = rand() % N + 1;
+	int i = rand() % 6;
+	float f = (rand() / (float) RAND_MAX);
+	float s = 0.85f;
+	float pal[3] = {1 - s, 1 - s * f, 1 - s * (1 - f)};
+	col[0] = pal[i % 3];
+	col[1] = pal[i > 2 ? (i + 1) % 3 : (i + 2) % 3];
+	col[2] = pal[i > 2 ? (i + 2) % 3 : (i + 1) % 3];
+    } // resetGeyser
 };
 
-void resetGeyser(GeyserState& g) { 
-    g.par1 = rand() % 2; // random numbers not guaranteed to be uniform here
-    g.par2 = rand() % 2;
-    g.dist = rand() % N + 1; 
-    int i = rand() % 6;
-    float f = (rand() / (float) RAND_MAX);
-    float s = 0.85f;
-    float pal[3] = {1 - s, 1 - s * f, 1 - s * (1 - f)};
-    g.col[0] = pal[i % 3];
-    g.col[1] = pal[i > 2 ? (i + 1) % 3 : (i + 2) % 3];
-    g.col[2] = pal[i > 2 ? (i + 2) % 3 : (i + 1) % 3];
-} // resetGeyser
-
-void updateGeyser(FluidSim& sim, GeyserState& g, float now) {
+void updateGeyser(FluidSim& sim, DyeSim& dye, GeyserState& g, float now) {
     if (now < g.next) return;
     if (now >= g.next + 3000) {
-	g.next = now + 500 + (rand() / (float) RAND_MAX) * 1500;
-	resetGeyser(g);
+	g.resetGeyser(now);
 	return;
     }
 
@@ -161,14 +186,17 @@ void updateGeyser(FluidSim& sim, GeyserState& g, float now) {
 
     for (int k = -jetSize; k <= jetSize; k++) {
 	int dd = clamp(g.dist + k, 1, N);
-	int id = g.par1 ? IX(dd, nf) : IX(nf, dd);
-
-	sim.dr[id] += vol * g.col[0];
-	sim.dg[id] += vol * g.col[1];
-	sim.db[id] += vol * g.col[2];
-
-	if (g.par1) sim.v[id] += jet;
-	else	    sim.u[id] += jet;
+	if (g.par1) {
+	    dye.dr(dd, nf) += vol * g.col[0];
+	    dye.dg(dd, nf) += vol * g.col[1];
+	    dye.db(dd, nf) += vol * g.col[2];
+	    sim.v(dd, nf) += jet;
+	} else {
+	    dye.dr(nf, dd) += vol * g.col[0];
+	    dye.dg(nf, dd) += vol * g.col[1];
+	    dye.db(nf, dd) += vol * g.col[2];
+	    sim.u(nf, dd) += jet;
+	}
     }
 } // updateGeyser
 
@@ -177,12 +205,9 @@ struct MouseState {
     float ppmx = -1, ppmy = -1;
 };
 
-void pointerMotion(SDL_Event& e, SDL_Window* win, FluidSim& sim, MouseState& m) {
-    int w, h;
-    SDL_GetWindowSize(win, &w, &h);
-
-    float gx = 1 + (e.motion.x / (float)w) * (N - 1);
-    float gy = 1 + (e.motion.y / (float)h) * (N - 1);
+void pointerMotion(SDL_Event& e, float winW, float winH, FluidSim& sim, MouseState& m) {
+    float gx = 1 + (e.motion.x / winW) * (N - 1);
+    float gy = 1 + (e.motion.y / winH) * (N - 1);
 
     if (m.pmx < 0) {
         m.pmx = m.ppmx = gx;
@@ -204,11 +229,10 @@ void pointerMotion(SDL_Event& e, SDL_Window* win, FluidSim& sim, MouseState& m) 
 
     for (int i = i0; i <= i1; i++) {
 	for (int j = j0; j <= j1; j++) {
-	    int id = IX(i, j);
-	    float dist = hypot(ii - i, jj - j);
-	    float wgt = exp(-2 * dist / r);
-	    sim.u[id] += dx * wgt;
-	    sim.v[id] += dy * wgt;
+	    float dist2 = (ii - i) * (ii - i) + (jj - j) * (jj - j);
+	    float wgt = exp(-2 * dist2 / r);
+	    sim.u(i, j) += dx * wgt;
+	    sim.v(i, j) += dy * wgt;
 	}
     }
 }
@@ -225,27 +249,24 @@ void initialise(SDL_Window*& win, SDL_Renderer*& ren, SDL_Texture*& tex) {
 
     tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, N, N);
 
-//    SDL_SetTextureScaleMode(tex, SDL_SCALEMODE_PIXELART); 
-    SDL_SetTextureScaleMode(tex, SDL_SCALEMODE_LINEAR); 
+//    SDL_SetTextureScaleMode(tex, SDL_SCALEMODE_PIXELART);
+    SDL_SetTextureScaleMode(tex, SDL_SCALEMODE_LINEAR);
 }
 
-void render(SDL_Renderer* ren, SDL_Texture* tex, SDL_Window* win, FluidSim& sim, vector<unsigned char>& pixels) {
+void render(SDL_Renderer* ren, SDL_Texture* tex, float winW, float winH, const DyeSim& dye, vector<unsigned char>& pixels) {
     for (int j = 1; j <= N; j++) {
 	for (int i = 1; i <= N; i++) {
-	    int id = IX(i, j);
 	    int k = ((j - 1) * N + (i - 1)) * 4;
-	    pixels[k]     = clamp((int) sim.dr[id], 0, 255);
-	    pixels[k + 1] = clamp((int) sim.dg[id], 0, 255);
-	    pixels[k + 2] = clamp((int) sim.db[id], 0, 255);
+	    pixels[k]     = clamp((int) dye.dr(i, j), 0, 255);
+	    pixels[k + 1] = clamp((int) dye.dg(i, j), 0, 255);
+	    pixels[k + 2] = clamp((int) dye.db(i, j), 0, 255);
 	    pixels[k + 3] = 255;
 	}
     }
 
     SDL_UpdateTexture(tex, NULL, pixels.data(), N * 4);
 
-    int w, h;
-    SDL_GetWindowSize(win, &w, &h);
-    SDL_FRect dst = {0, 0, (float) w, (float) h};
+    SDL_FRect dst = {0, 0, winW, winH};
 
     SDL_RenderClear(ren);
     SDL_RenderTexture(ren, tex, NULL, &dst);
@@ -257,14 +278,18 @@ int main() {
     SDL_Renderer* ren;
     SDL_Texture* tex;
     initialise(win, ren, tex);
+    int w, h;
+    SDL_GetWindowSize(win, &w, &h);
+    float winW = (float) w;
+    float winH = (float) h;
 
     FluidSim sim;
+    DyeSim dye;
     vector<unsigned char> pixels(N * N * 4);
 
     MouseState mouse;
     GeyserState geyser;
-    resetGeyser(geyser);
-    geyser.next = SDL_GetTicks() + 500 + (rand() / (float) RAND_MAX) * 1500;
+    geyser.resetGeyser((float) SDL_GetTicks());
 
     float last = SDL_GetTicks() / 1000.0;
     bool running = true;
@@ -273,19 +298,23 @@ int main() {
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) running = false;
+	    if (e.type == SDL_EVENT_WINDOW_RESIZED) {
+		winW = (float) e.window.data1;
+		winH = (float) e.window.data2;
+	    }
             if (e.type == SDL_EVENT_MOUSE_MOTION)
-                pointerMotion(e, win, sim, mouse);
+                pointerMotion(e, winW, winH, sim, mouse);
         }
 
         float now = SDL_GetTicks() / 1000.0;
         float dt = min(now - last, 1.0f / 30);
         last = now;
 
-        updateGeyser(sim, geyser, now * 1000);
+        updateGeyser(sim, dye, geyser, now * 1000);
         sim.velocityStep(dt);
-        sim.densityStep(dt);
+        dye.densityStep(sim.u, sim.v, dt);
 
-        render(ren, tex, win, sim, pixels);
+        render(ren, tex, winW, winH, dye, pixels);
     }
 
     SDL_Quit();
